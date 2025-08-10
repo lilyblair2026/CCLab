5c404ee2e4270d6696b198cd4e17eaefadda8d2c
lilyblair2026 <lc4683@nyu.edu>
2025-08-11 00:20:57 +0800

    Create sketch.js

---
+++ Class Codes/class-08-1-arrays/sketch.js
@@ -0,0 +1,28 @@
+let colors = [
+  "Aquamarine",
+  "Chartreuse",
+  "CornflowerBlue",
+  "Cornsilk",
+  "DarkOrchid",
+  "DarkTurquoise",
+  "DeepPink"
+]; // array!
+
+
+function setup() {
+  let canvas = createCanvas(800, 500);
+  canvas.parent("p5-canvas-container");
+}
+
+
+function draw() {
+  background(220);
+
+
+  for (let i = 0; i < colors.length; i++) {
+    let x = 100;
+    let y = 100 + i * 60;
+    textSize(40);
+    text(colors[i], x, y);
+  }
+}
